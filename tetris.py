426ae689b003e5e2cae0bf624ec778e3dadc46a1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-08-21 10:51:13 +0200

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -15,3 +15,49 @@ class Juego(object):
             'titulo': pygame.font.Font('fressansbold.ttf', 100),
         }
         pygame.display.set_caption('Aplicación Tetris')
+
+def start(self):
+    self._mostrarTexto('Tetris', CENTRO_VENTANA, fuente='titulo')
+    self.mostrar_Texto('Pulse una tecla...', POS)
+    self.espera()
+
+def stop(self):
+    sel.mostrarTexto('Ha perdido', CENTRO_VENTANA, fuente='titulo')
+    self.espera()
+    self.salir()
+
+def _mostrarTexto(self, texto, posicion, color=9, fuente='predeterminada'):
+    print("Mostrar  texto")
+    fuente = self.fuentes.get(fuente, self.fuentes['predeterminada'])
+    color = COLORES.get(color, COLORES[9])
+    repres = fuente.render(texto, True, color)
+    rect = repres.get_rect()
+    rect.center = posicion
+    self.surface.blit(repres, rect)
+
+def _espera(self):
+    print("Espera")
+    while self._getEvent() == None:
+        self._restituir()
+
+def restituir(self):
+    pygame.display.update()
+    self.clock.tick()
+
+def _getEvent(self):
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            self.salir()
+        if event.type==KEYUP:
+            if event.key==K_ESCAPE:
+                self._salir()
+        if event.type==KEYDOWN:
+            if event.key==K_ESCAPE:
+                continue
+            return event.key
+
+def salir(self):
+    print("Salir")
+    pygame.quit()
+    sys.exit()
+
